5acb2bba946f0a8262a29dbf5b25df986989495e
goisot <hugo.goisot@lyceeastier.com>
2023-11-16 09:13:27 +0100

    amélioration 2 fini début de l'amélioration 3

--- src/fr/btsiel/Ihm.java
+++ src/fr/btsiel/Ihm.java
@@ -8,7 +8,9 @@ public class Ihm {
         int saisieVictoireAdevrsaire;
         int capitalPointDepart = 0;
         int nombreVictoireMax = 0;
-        int nombrevictoireTotal = 0;
+        int nombrevictoireTotal ;
+        int nombreDefaite;
+        int echelonAdversaire;
         int nombreDefaiteEchelonEgale = 0;
         int nombreDefaiteEchelonInferieur = 0;
         int nombreDefaiteEchelon2 = 0;
@@ -17,10 +19,14 @@ public class Ihm {
         int pointMax = 0;
         int ecart;
         int bonusVictory;
+        int compteur_G = 0;
+        int compteur_i = 0;
+        int compteur_e = 0;
         final int NC = 1, _40 = 2, _30_5 = 3, _30_4 = 4, _30_3 = 5;
         final int _30_2 = 6, _30_1 = 7, _30 = 8, _15_5 = 9, _15_4 = 10;
         boolean flag = true;
 
+//        demande le rang du joueur
         System.out.println("""
                     veuillez donner votre classement parmis les choix suivant :
                     1| Non-Classé                   6| 30/2
@@ -31,8 +37,10 @@ public class Ihm {
                     """);
         saisieClassement = In.readInteger();
 
+//      on fait une boucle do-while car on fais au moins une fois le programme
         do{
-
+            System.out.println("votre classement est de "+saisieClassement);
+//            attribution de ses point de depart et de ses victoire max celon son rang
             switch (saisieClassement) {
                 case NC:
                     nombreVictoireMax = 5;
@@ -99,6 +107,7 @@ public class Ihm {
             System.out.println("veuillez donner votre nombre de victoire ");
             saisieVictoire = In.readInteger();
 
+//            viens regarder si le nombre de victoire donner par le joueur est bien inferieur ou egale a la valeur de victoire max de son rang
             if (saisieVictoire > nombreVictoireMax) {
                 nombrevictoireTotal = nombreVictoireMax;
                 System.out.println("malgré vos nombreuse victoire nous devons réduire ce nombre à " + nombreVictoireMax);
@@ -107,19 +116,30 @@ public class Ihm {
                 System.out.println("votre nombre de victoire et de " + nombrevictoireTotal);
             }
 
-//            calcul des points gagner grace a ses victoires et a ses victoire bonus
+//            calcul des victoire bonus
             if (nombrevictoireTotal >= nombreVictoireMax){
                 System.out.println("nous allons calculer votre bonus de victoire");
-                System.out.println("veuillez donner votre nombre de defaite a echelon égale ");
-                nombreDefaiteEchelonEgale = In.readInteger();
-
-                System.out.println("veuillez donner votre nombre de défaite a echelon inférieur ");
-                nombreDefaiteEchelonInferieur = In.readInteger();
-
-                System.out.println("veuillez donner votre nombre de defaite a echelon inferieur 2");
-                nombreDefaiteEchelon2 = In.readInteger();
-
-                bonusVictory = nombrevictoireTotal - nombreDefaiteEchelonEgale - (2 * nombreDefaiteEchelonInferieur) - ( 5* nombreDefaiteEchelon2);
+                System.out.println("veuillez donner votre nombre de défaite ");
+                nombreDefaite = In.readInteger();
+                for (int i = 0; i < nombreDefaite; i++) {
+                    System.out.println("veuillez donner l'echelon de l'adversaire qui vous as battu ");
+                    echelonAdversaire = In.readInteger();
+                    if (echelonAdversaire == saisieClassement){
+                        nombreDefaiteEchelonEgale++;
+                        compteur_e++;
+                        System.out.println("vous avez "+ compteur_e+" point dans le e ( qui équivaut a une defaite a echelon egale) ");
+                    } else if (echelonAdversaire +1 == saisieClassement) {
+                        nombreDefaiteEchelonInferieur++;
+                        compteur_i++;
+                        System.out.println("vous avez "+ compteur_i+" point dans le i (qui équivaut a une defaite a un echelon inféreiru)");
+                    }else if (echelonAdversaire +2 == saisieClassement){
+                        nombreDefaiteEchelon2++;
+                        compteur_G++;
+                        System.out.println("vous avez "+ compteur_G+ " point dans le G (qui équivaut a une defaite a deux echelon inferieur)");
+                    }
+                }
+                bonusVictory = saisieVictoire - nombreDefaiteEchelonEgale - (2 * nombreDefaiteEchelonInferieur) - ( 5* nombreDefaiteEchelon2);
+                System.out.println("vous avez le droit à "+ bonusVictory + " victoire bonus");
 
                if(bonusVictory >= 25){
                    nombrevictoireTotal += 6;
@@ -135,6 +155,8 @@ public class Ihm {
                    nombrevictoireTotal += 1;
                }
             }
+
+//            calcul des points gagner grâce a ses victoires
             for (int i = 0; i < nombrevictoireTotal; i++) {
                 System.out.println("""
                         veuillez donner le classement de votre adversaire parmis les choix suivant :
@@ -161,13 +183,13 @@ public class Ihm {
                 }
                 System.out.println("votre nombre de points grâce a cette victoire est de " + pointGagner);
                 capitalPointDepart += pointGagner;
-
             }
-//            affichage du rang
+
+//          affichage du changement de classe (s'il y en a un) et mis en arret du programme s'il descend ou ne change pas de classement
             System.out.println("votre nombre de point totale est de " + capitalPointDepart);
             if (capitalPointDepart >= pointMax) {
                 saisieClassement++;
-                System.out.println("vous avez augmenter de 1 rang au niveau du classement");
+                System.out.println("vous avez augmenter de 1 rang au niveau du classement\n");
             } else if (capitalPointDepart <= pointMin) {
                 saisieClassement--;
                 System.out.println("vous avez diminué de 1 rang au niveau du classement");
